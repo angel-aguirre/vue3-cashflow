8be4460fe1a5fd6b0d5f783f60a85492d20958cb
Angel Aguirre <angelaguirrel@hotmail.com>
2022-05-10 14:16:13 -0500

    Creacion del componente SplashScreen

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
 <template>
-    <Home />
+    <Suspense>
+        <template #default>
+            <Home />
+        </template>
+        <template #fallback>
+            <SplashScreen />
+        </template>
+    </Suspense>
 </template>
 
 <script setup>
-import Home from '@/components/Home.vue';
+import SplashScreen from '@/components/SplashScreen.vue';
+import { defineAsyncComponent } from 'vue';
+
+const Home = defineAsyncComponent(
+    () =>
+        new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(import('@/components/Home.vue'));
+            }, 2500);
+        })
+);
 </script>
 
-<style></style>
+<style>
+html,
+body,
+.app {
+    min-height: 100vh;
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex justify-center items-center flex-col h-screen">
+        <img src="@/assets/img/cashflow-icon.svg" alt="Logo" />
+        <h1 class="text-brand-green mt-2 text-xl">
+            <span class="text-brand-blue">Cash</span>Flow
+        </h1>
+    </div>
+</template>
