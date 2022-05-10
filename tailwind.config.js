4206e0876399ea1d69e002150ce1eb8ab78b1d61
Angel Aguirre <angelaguirrel@hotmail.com>
2022-05-10 14:10:59 -0500

    Se agrego Tailwind al proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                'brand-green': '#04b500',
+                'brand-blue': '#0689b0',
+            },
+        },
+    },
+    plugins: [],
+    purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+};
