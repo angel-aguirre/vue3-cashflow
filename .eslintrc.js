e8460fe69748648b3a68e4dedb71c23d4efbac98
Angel Aguirre <angelaguirrel@hotmail.com>
2022-05-10 12:09:28 -0500

    Estructura inicial del proyecto

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ['plugin:vue/vue3-essential', 'plugin:prettier/recommended'],
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+    },
+    rules: {
+        indent: ['error', 4],
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
+    overrides: [
+        {
+            files: ['src/components/**/*.vue'],
+            rules: {
+                'vue/multi-word-component-names': 0,
+            },
+        },
+    ],
 };
