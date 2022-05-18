88ff18cd77af32a4d4ce221c2b4fc3e03bdd8832
Angel Aguirre <angelaguirrel@hotmail.com>
2022-05-17 20:45:40 -0500

    Se agrego el formulario para agregar un nuevo movimiento

--- src/components/Action.vue
+++ src/components/Action.vue
@@ -6,9 +6,61 @@
         Agregar movimiento
     </button>
     <teleport to="#app">
-        <Modal @close="showModal = false" v-show="showModal"
-            >Formulario del modal</Modal
-        >
+        <Modal @close="showModal = false" v-show="showModal">
+            <form @submit.prevent="submit" class="text-xl w-full">
+                <div class="flex justify-between flex-col py-4 px-6">
+                    <label class="mb-2">Titulo</label>
+                    <input
+                        v-model="title"
+                        class="text-xl border-2 border-brand-blue rounded-lg p-2"
+                        type="text"
+                    />
+                </div>
+                <div class="flex justify-between flex-col py-4 px-6">
+                    <label class="mb-2">Monto</label>
+                    <input
+                        v-model="amount"
+                        class="text-xl border-2 border-brand-blue rounded-lg p-2 text-right"
+                        type="number"
+                    />
+                </div>
+                <div class="flex justify-between flex-col py-4 px-6">
+                    <label class="mb-2">Descripcion</label>
+                    <textarea
+                        v-model="description"
+                        rows="4"
+                        class="text-xl border-2 border-brand-blue rounded-lg p-2"
+                    ></textarea>
+                </div>
+                <div class="flex justify-between flex-col py-4 px-6">
+                    <label class="flex items-center mt-2">
+                        <input
+                            v-model="movementType"
+                            class="appearance-none w-5 h-5 text-brand-blue border-2 border-brand-blue rounded-[50%] checked:bg-brand-blue"
+                            value="Ingreso"
+                            type="radio"
+                        />
+                        <span class="mt-1 ml-2">Ingreso</span>
+                    </label>
+                    <label class="flex items-center mt-2">
+                        <input
+                            v-model="movementType"
+                            class="appearance-none w-5 h-5 text-brand-blue border-2 border-brand-blue rounded-[50%] checked:bg-brand-blue"
+                            value="Gasto"
+                            type="radio"
+                        />
+                        <span class="mt-1 ml-2">Gasto</span>
+                    </label>
+                </div>
+                <div class="py-0 px-6 pb-2">
+                    <button
+                        class="text-white text-xl bg-brand-blue border-none w-full py-6 px-14 rounded-[60px] box-border"
+                    >
+                        Agregar
+                    </button>
+                </div>
+            </form>
+        </Modal>
     </teleport>
 </template>
 
@@ -17,4 +69,12 @@ import { ref } from 'vue';
 import Modal from '@/components/Modal.vue';
 
 const showModal = ref(false);
+const title = ref('');
+const amount = ref(0);
+const description = ref('');
+const movementType = ref('Ingreso');
+
+const submit = () => {
+    showModal.value = false;
+};
 </script>
